250fd5ed4f7ab2506e73c7fbb937715672f68f90
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2020-04-23 23:39:22 +0100

    Updated style

--- main.js
+++ main.js
@@ -85,8 +85,10 @@ const renderForecast = (day) => {
   $weatherDiv.append(weatherContent);
   // Add your code here:
 };
+const starter = document.getElementById("starter");
 
 const executeSearch = () => {
+  starter.remove();
   $venueDivs.forEach((venue) => venue.empty());
   $weatherDiv.empty();
   $destination.empty();
@@ -100,21 +102,23 @@ const executeSearch = () => {
 
 const createVenueHTML = (name, location, iconSource) => {
   return `<h2>${name}</h2>
-  <img class="venueimage" src="${iconSource}"/>
-  <h3>Address:</h3>
-  <p>${location.address}</p>
-  <p>${location.city}</p>
-  <p>${location.country}</p>`;
+    <img class="venueimage" src="${iconSource}"/>
+    <h3>Address:</h3>
+    <p>${location.address}</p>
+    <p>${location.city}</p>
+    <p>${location.country}</p>`;
 };
 
 const createWeatherHTML = (currentDay) => {
   console.log(currentDay);
   return `<h2>${weekDays[new Date().getDay()]}</h2>
-		<h2>Temperature: ${kelvinToFahrenheit(currentDay.main.temp)}&deg;F</h2>
-		<h2>Condition: ${currentDay.weather[0].description}</h2>
-  	<img src="https://openweathermap.org/img/wn/${
-      currentDay.weather[0].icon
-    }@2x.png">`;
+          <h2>Temperature: ${kelvinToFahrenheit(
+            currentDay.main.temp
+          )}&deg;F</h2>
+          <h2>Condition: ${currentDay.weather[0].description}</h2>
+        <img src="https://openweathermap.org/img/wn/${
+          currentDay.weather[0].icon
+        }@2x.png">`;
 };
 
 const kelvinToFahrenheit = (k) => (((k - 273.15) * 9) / 5 + 32).toFixed(0);
